c57e99f49cd4686e15dc988dd4ebdeeafd691bf3
Mathys Cogne Foucault <mathys.cognef@gmail.com>
2023-05-20 17:20:56 +0200

    Init ARGENT BANK

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// IMPORTS 
+import { Route, Routes } from "react-router-dom";
 
+// PAGES
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import User from "./pages/User";
+import Error from "./pages/Error";
+
+// LAYOUTS
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
+
+// COMPONENTS
+import EditUser from "./components/EditUser";
+import Transactions from "./components/Transactions";
+
+// STYLES
+import "./styles/App.scss";
+
+
+// ROUTES & HEADER / FOOTER
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/user:id" element={<User />} />
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+
+      <Footer />
+    </>
   );
 }
 
